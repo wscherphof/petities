967287a2a441be2dbfd5bf0f3d986f19f2211821
Wouter Scherphof <wouter.scherphof@gmail.com>
2017-04-03 13:15:31 +0200

    More data fields
    TODO: the list of websites

---
+++ messages/petition.go
@@ -0,0 +1,103 @@
+package messages
+
+import (
+	msg "github.com/wscherphof/essix/messages"
+)
+
+func init() {
+	msg.Key("signatures").
+		Set("nl", "ondertekeningen").
+		Set("en", "signatures")
+
+	msg.Key("Petition").
+		Set("nl", "Petitie").
+		Set("en", "Petition")
+
+	msg.Key("We").
+		Set("nl", "Wij").
+		Set("en", "We")
+
+	msg.Key("observe").
+		Set("nl", "constateren").
+		Set("en", "observe")
+
+	msg.Key("and request").
+		Set("nl", "en verzoeken").
+		Set("en", "and request")
+
+	msg.Key("Sign this petition").
+		Set("nl", "Onderteken deze petitie").
+		Set("en", "Sign this petition")
+
+	msg.Key("Name").
+		Set("nl", "Naam").
+		Set("en", "Name")
+
+	msg.Key("Email address").
+		Set("nl", "E-mailadres").
+		Set("en", "Email address")
+
+	msg.Key("City").
+		Set("nl", "Woonplaats").
+		Set("en", "Place of residence")
+
+	msg.Key("Name placeholder").
+		Set("nl", "Voorletter en achternaam minstens").
+		Set("en", "Initial and surname at least")
+
+	msg.Key("Email placeholder").
+		Set("nl", "gebruikersnaam@iets.iets").
+		Set("en", "username@something.something")
+
+	msg.Key("City placeholder").
+		Set("nl", "Waar u nu woont, op het tijdstip van ondertekenen").
+		Set("en", "Where you live now, at the time of signing")
+
+	msg.Key("Visible").
+		Set("nl", "mijn naam en woonplaats mogen publiek zichtbaar zijn onder de petitie.").
+		Set("en", "Publish my name visibly below the petition.")
+
+	msg.Key("Submit signature").
+		Set("nl", "Ja, ik onderteken deze petitie").
+		Set("en", "Yes, I sign this petition")
+
+	msg.Key("Details").
+		Set("nl", "Details").
+		Set("en", "Details")
+
+	msg.Key("Addressed to").
+		Set("nl", "Ontvanger").
+		Set("en", "Addressed to")
+
+	msg.Key("Petition desk").
+		Set("nl", "Petitieloket").
+		Set("en", "Petition desk")
+
+	msg.Key("Closing date").
+		Set("nl", "Einddatum").
+		Set("en", "Closing date")
+
+	msg.Key("Answer expected").
+		Set("nl", "Antwoord verwacht").
+		Set("en", "Answer expected")
+
+	msg.Key("Date format").
+		Set("nl", "2-1-2006").
+		Set("en", "1/2/2006")
+
+	msg.Key("Status").
+		Set("nl", "Status").
+		Set("en", "Status")
+
+	msg.Key("Signable").
+		Set("nl", "Ondertekenbaar").
+		Set("en", "Signable")
+
+	msg.Key("Websites").
+		Set("nl", "Websites").
+		Set("en", "Websites")
+
+	msg.Key("History").
+		Set("nl", "Geschiedenis").
+		Set("en", "History")
+}
--- model/petition.go
+++ model/petition.go
@@ -3,30 +3,97 @@ package model
 import (
 	"github.com/wscherphof/entity"
 	"github.com/wscherphof/msg"
+	"time"
 )
 
 type Petition struct {
 	*entity.Base
-	Caption msg.MessageType
+	Address       msg.MessageType
+	Desk          msg.MessageType
+	Closed        time.Time
+	Answered      time.Time
+	Status        string
+	NumSignatures msg.MessageType
+	Caption       msg.MessageType
+	Intro         msg.MessageType
+	We            msg.MessageType
+	Observations  []msg.MessageType
+	Requests      []msg.MessageType
 }
 
 func init() {
 	entity.Register(&Petition{})
 	groningen := InitPetition("groningen")
-	if err, empty := groningen.Read(groningen); err != nil && !empty {
-		panic(err)
-	}
-	groningen.Caption.
+	groningen.Address = msg.New().
+		Set("nl", "Tweede Kamer").
+		Set("en", "House of Representatives")
+	groningen.Desk = msg.New().
+		Set("nl", "Nederland").
+		Set("en", "The Netherlands")
+	format := "2006-Jan-02"
+	groningen.Closed, _ = time.Parse(format, "2017-May-08")
+	groningen.Answered, _ = time.Parse(format, "2017-May-18")
+	groningen.Status = "Signable"
+	groningen.NumSignatures = msg.New().
+		Set("nl", "179.538").
+		Set("en", "179,538")
+	groningen.Caption = msg.New().
 		Set("nl", "Laat Groningen niet zakken").
 		Set("en", "Don’t let Groningen down")
+	groningen.Intro = msg.New().
+		Set("nl", "Aardbevingen, veroorzaakt door gaswinning, hebben het leefklimaat in de provincie Groningen voor een groot deel verwoest. Schadeafhandeling stuit op spanning tussen rechtvaardig en rechtmatig. Woningen en gebouwen zijn onveilig. Gedupeerden voelen zich vaak ‘gevangen in een onveilige gevangenis’. - Freek de Jonge").
+		Set("en", "Earthquakes caused by gas extraction, have destroyed a large part of the living environment in the province of Groningen. The process of claiming damages is causing tension between what is righteous and what is lawful. Homes and buildings are unsafe. And victims often feel ‘trapped in an unstable prison’. - Freek de Jonge")
+	groningen.We = msg.New().
+		Set("nl", "Nederlanders, en overige gebruikers van Gronings gas, solidair met de gedupeerde Groningers.").
+		Set("en", "The people living in the Netherlands, and other users of gas coming from Groningen, stand by the victims of Groningen.")
+	groningen.Observations = append(groningen.Observations,
+		msg.New().
+			Set("nl", "Dat de aardgaswinning in Groningen tot een catastrofe leidt").
+			Set("en", "That gas extraction in Groningen is leading to a catastrophe"),
+		msg.New().
+			Set("nl", "Dat verwevenheid van belangen van overheid en bedrijfsleven en leveringsafspraken spotten met de veiligheid van de inwoners").
+			Set("en", "That the interdependence of interests of government and industry and supply arrangements mock the safety of the local residents"),
+		msg.New().
+			Set("nl", "Dat zowel vaststelling als afhandeling van de aardbevingsschade door de NAM van de gedupeerde een machteloos, tot eindeloos afwachten gedwongen, slachtoffer maakt").
+			Set("en", "That the manner in which the earthquake damage is determined and settled by the NAM leaves victims feeling powerless and forced to wait endlessly for a resolution"),
+		msg.New().
+			Set("nl", "Dat de consequenties van toekomstige bevingen door overheid en bestuurders onderschat worden").
+			Set("en", "That the consequences of future earthquakes are grossly understated by government and directors"),
+	)
+	groningen.Requests = append(groningen.Requests,
+		msg.New().
+			Set("nl", "Afbouwplan versneld stoppen gaswinning").
+			Set("en", "A speed up plan to reduce gas extraction"),
+		msg.New().
+			Set("nl", "'Generaal pardon' voor alle in behandeling zijnde schades").
+			Set("en", "A ‘general pardon’ for all damage claims currently pending"),
+		msg.New().
+			Set("nl", "Daarna omkering bewijslast in een onafhankelijk schadeproces ").
+			Set("en", "Then reverse burden of proof in an independent claims process"),
+		msg.New().
+			Set("nl", "Uitkoopregeling voor iedereen die het gebied wil verlaten ").
+			Set("en", "A buy-out settlement for everyone wanting to leave the region"),
+		msg.New().
+			Set("nl", "Scheiding gas en staat").
+			Set("en", "The separation of gas and state"),
+		msg.New().
+			Set("nl", "Herstel gemeenschappelijk overleg").
+			Set("en", "Resume communal discussion"),
+		msg.New().
+			Set("nl", "Gasbaten investeren").
+			Set("en", "Invest gas profits"),
+		msg.New().
+			Set("nl", "Deltaplan dat Groningen koploper maakt in transitie van economie en energievoorziening").
+			Set("en", "Delta plans making Groningen a leader in economic and energy transition"),
+		msg.New().
+			Set("nl", "Internationaal kenniscentrum voor milieu in Groningen vestigen.").
+			Set("en", "Set up international knowledge center for environment in Groningen"),
+	)
 	if err := groningen.Update(groningen); err != nil {
 		panic(err)
 	}
 }
 
 func InitPetition(id string) *Petition {
-	return &Petition{
-		Base:    &entity.Base{ID: id},
-		Caption: msg.New(),
-	}
+	return &Petition{Base: &entity.Base{ID: id}}
 }
