6f800e96008e6acf5af9f11a8bd3846cc3fa77fd
Wouter Scherphof <wouter.scherphof@gmail.com>
2017-04-12 23:16:32 +0200

    loader.io support

--- routes/home.go
+++ routes/home.go
@@ -10,4 +10,8 @@ func init() {
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		template.Handle("petition", "Home", "")(w, r, ps)
 	})
+
+	router.GET("/loaderio-:code", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		http.Redirect(w, r, "/static/loaderio-"+ps.ByName("code")+".txt", http.StatusSeeOther)
+	})
 }
