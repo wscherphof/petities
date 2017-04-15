6a040f79541f90027f7a7d6a43f7056666989e8f
Wouter Scherphof <wouter.scherphof@gmail.com>
2017-04-15 22:04:22 +0200

    cleanup

--- routes/home.go
+++ routes/home.go
@@ -2,12 +2,11 @@ package routes
 
 import (
 	"github.com/julienschmidt/httprouter"
-	"github.com/wscherphof/essix/template"
 	"net/http"
 )
 
 func init() {
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		template.Handle("petition", "Home", "")(w, r, ps)
+		http.Redirect(w, r, "/petition?id=groningen", http.StatusSeeOther)
 	})
 }
--- routes/petition.go
+++ routes/petition.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/wscherphof/essix/template"
 	"github.com/wscherphof/petities/routes/petition"
 )
 
@@ -10,9 +11,11 @@ func init() {
 	router.POST("/signature", petition.Signature)
 	router.GET("/signature/post", petition.Signature)
 
+	router.GET("/provision", template.Handle("petition", "ProvisionForm", ""))
 	router.POST("/provision", petition.Provision)
 	router.GET("/provision/post", petition.Provision)
 
+	router.GET("/synchronise", template.Handle("petition", "SynchroniseForm", ""))
 	router.POST("/synchronise", petition.Synchronise)
 	router.GET("/synchronise/post", petition.Synchronise)
 }
