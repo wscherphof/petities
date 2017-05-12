f7b7a289f9b529ad792fc9881624afaaf13a88d7
Wouter Scherphof <wouter.scherphof@gmail.com>
2017-05-12 09:54:17 +0200

    /provision only in test env

--- routes/petition.go
+++ routes/petition.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
+	"github.com/wscherphof/env"
 	"github.com/wscherphof/essix/ratelimit"
 	"github.com/wscherphof/essix/template"
 	"github.com/wscherphof/petities/routes/petition"
 )
+
+var test = (env.Get("GO_ENV", "") == "test")
 
 func init() {
 	router.GET("/petition", petition.Petition)
@@ -17,11 +20,13 @@ func init() {
 	router.PUT("/signature/confirm", petition.Confirm)
 	router.GET("/signature/confirm/put", petition.Confirm)
 
-	router.GET("/provision", template.Handle("petition", "ProvisionForm", ""))
-	router.POST("/provision", petition.Provision)
-	router.GET("/provision/post", petition.Provision)
-
 	router.GET("/synchronise", template.Handle("petition", "SynchroniseForm", ""))
 	router.POST("/synchronise", petition.Synchronise)
 	router.GET("/synchronise/post", petition.Synchronise)
+
+	if test {
+		router.GET("/provision", template.Handle("petition", "ProvisionForm", ""))
+		router.POST("/provision", petition.Provision)
+		router.GET("/provision/post", petition.Provision)
+	}
 }
